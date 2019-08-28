d15d4b552ba7664552aac54916f4ee3acc1b2dd4
Andersen Fan <as181920@gmail.com>
2019-08-29 00:01:11 +0800

    Fix base scope in chained activity generation

--- app/services/omni_activity/chained_activity_service.rb
+++ app/services/omni_activity/chained_activity_service.rb
@@ -4,40 +4,39 @@ module OmniActivity
 
     DEFAULT_TRACKING_LEVELS = 2
 
-    def generate(actor, previous_id, options={})
-      previous = previous_id.present? ? Activity.find_by(id: previous_id) : nil
+    def generate(target:, actor:, name:, parent_id: nil, options: {})
+      parent = parent_id.present? ? Activity.find_by(id: parent_id) : nil
+      return parent if parent && ([parent.target, parent.actor, parent.name] == [target, actor, name])
 
-      if previous&.actor == actor
-        previous
-      else
-        detect_matching_activity(actor, previous, options) || create_chained_activity(actor, previous, options)
-      end
+      base_scope = Activity.where(target: target, actor: actor, name: name)
+      matching_scope(base_scope, parent, options).first_or_create \
+        parent_id: parent&.id,
+        occurred_at: options[:occurred_at].presence,
+        description: options[:description],
+        options: options.except(:occurred_at, :description).except(:tracking_level)
     end
 
     private
-      def detect_matching_activity(actor, previous, options)
-        base_scope = Activity.where(actor: actor)
+      def matching_scope(base_scope, parent, options)
         join_sources = []
         (options[:tracking_level].presence || DEFAULT_TRACKING_LEVELS).to_i.times.map{ |idx| Activity.arel_table.alias(get_activity_table_alias(idx)) }.each_cons(2).map.with_index do |(tp, tn), idx|
-          nth_previous = previous&.nth_parent(idx)
-          if nth_previous.nil?
-            return base_scope.joins(join_sources).where(get_activity_table_alias(idx) => {parent_id: nil}).first
+          nth_parent = parent&.nth_parent(idx)
+          if nth_parent.nil?
+            return base_scope.joins(join_sources).where(get_activity_table_alias(idx) => {parent_id: nil})
           else
-            join_sources.push tp.left.join(tn).on(tp[:parent_id].eq(tn[:id]).and(tn[:actor_id].eq(nth_previous&.actor_id)).and(tn[:actor_type].eq(nth_previous&.actor_type))).join_sources
+            join_sources.push(
+              tp.left.join(tn).on(
+                tp[:parent_id].eq(tn[:id])
+                .and(tn[:actor_id].eq(nth_parent&.actor_id))
+                .and(tn[:actor_type].eq(nth_parent&.actor_type))
+                .and(tn[:target_id].eq(nth_parent&.target_id))
+                .and(tn[:target_type].eq(nth_parent&.target_type))
+                .and(tn[:name].eq(nth_parent&.name))
+              ).join_sources
+            )
           end
         end
-        base_scope.joins(join_sources).first
-      end
-
-      def create_chained_activity(actor, previous, options)
-        Activity.create \
-          parent_id: previous&.id,
-          actor: actor,
-          target: (options[:target] || previous.target),
-          name: (options[:name].presence || previous.name),
-          occurred_at: options[:occurred_at].presence,
-          description: options[:description],
-          options: options.except(:target, :name, :occurred_at, :description).except(:tracking_level)
+        base_scope.joins(join_sources)
       end
 
       def get_activity_table_alias(idx)
