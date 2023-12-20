1e6b89771dcf8466796b77c9fb72fd1ab5e9414e
Andersen Fan <as181920@gmail.com>
2023-12-20 20:34:52 +0800

    Chore: set ransackable on activity

--- app/models/omni_activity/activity.rb
+++ app/models/omni_activity/activity.rb
@@ -1,14 +1,22 @@
 module OmniActivity
   class Activity < ApplicationRecord
     belongs_to :actor, polymorphic: true
-    belongs_to :target, polymorphic: true,  optional: true
+    belongs_to :target, polymorphic: true, optional: true
 
     belongs_to :parent, class_name: "OmniActivity::Activity", foreign_key: :parent_id, optional: true
     has_many :children, class_name: "OmniActivity::Activity", foreign_key: :parent_id, dependent: nil
 
-    validates_presence_of :name, :occurred_at
+    validates :name, :occurred_at, presence: true
 
     before_validation :auto_set_occurred_at, on: :create
+
+    def self.ransackable_attributes(_auth_object = nil)
+      %w[id actor_type actor_id target_type target_id name occurred_at description parent_id]
+    end
+
+    def self.ransackable_associations(_auth_object = nil)
+      %w[actor target]
+    end
 
     def root
       node = self
@@ -16,9 +24,9 @@ module OmniActivity
       node
     end
 
-    def nth_parent(n)
+    def nth_parent(nth_number)
       node = self
-      n.times do
+      nth_number.times do
         node = node.parent
         break node if node.nil?
       end
@@ -26,6 +34,7 @@ module OmniActivity
     end
 
     private
+
       def auto_set_occurred_at
         self.occurred_at ||= Time.zone.now
       end
